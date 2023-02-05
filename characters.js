24cc6850e71e9eb5ccb3dc83f5e69960481cee4d
Teodor Petrov <teodor.petrov@abv.bg>
2023-02-05 14:26:45 +0200

    new files

---
+++ characters.js
@@ -0,0 +1,12 @@
+function characters(input) {
+    let text = input[0];
+
+    for (let i = 0; i < text.length; i++) {
+        let letter = text[i];
+        console.log(letter);
+        
+    }
+    
+}
+
+characters(["softuni"]);
